9f31d7b1425794391f1cb651caedc520acf1d25d
Emzzy241 <emzzyoluwole@gmail.com>
2022-11-10 02:33:21 +0100

    new feature added: the random feature

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 
         giphyPromise.then(function (myPromiseSuccess) {
 
-            // parsin the json, so I can be able to access it
+            // parsing the json, so I can be able to access it
             const giphyBody = JSON.parse(myPromiseSuccess);
 
             // console.log(myPromiseSuccess);
@@ -140,14 +140,9 @@ $(document).ready(function () {
 
         */
 
-      
+
     });
 
-
-/*
-    // Other features by the great giphy App
-
-    // saving up the 2 new features the app offers
 
     $("#other-features").click(function () {
 
@@ -158,111 +153,203 @@ $(document).ready(function () {
 
         // running a branch to determine which value user selected to determine which API CALL TO MAKE
 
-
         if (userPick === "random") {
+            let giphyRandomPromise = new Promise(function (randomPromiseResolved, randomPromiseRejected) {
 
-            // doing the same thing I did for the first app feature by the left 
-            // the only difference is the difference in the API call I'm making
+                // initiating an XMLhttprequest object for api and storing it in a variable
 
-            let giphyRandomRequest = new XMLHttpRequest();
+                let giphyRandomRequest = new XMLHttpRequest();
+                // this endpoint is for searching giphy for an mood user might be in 
 
-            // using another endpoint(the random endpoint): not the endpoint above but a different one 
-            // that will allow me ask giphy for a random gif
+                // with this enpoint I first protected my API key by hiding it with my environmental variables file and then I inputted the emotion user might be feeling, 
+                // At the beginning I set the search by telling giphy I want this particular endpoint to serach for gifs matching user's value 
 
-            const randomGiphyUrl = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`
 
-            giphyRandomRequest.onreadystatechange = function () {
-                console.log(this.readyState);
+                const giphyRandomUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userGif}&limit=1&offset=0&rating=g&lang=en`
 
-                if (this.readyState === 4 && this.status === 200) {
-                    const giphyRandomResponse = JSON.parse(this.responseText);
+                // using an .onload property here instead of the .onreadystatechange is really great as we no longer have to specify a readyState of 4, 
+                // and also it will be triggered once: (when the response has loaded) and not everytime the readyState has changes
 
-                    getRandomGifs(giphyRandomResponse);
+                // to summarize: using an onload propery here means we are only waiting for the request to load only
+                giphyRandomRequest.onload = function () {
+
+                    // running a branch to give me whas in the response property whether the Status = 200 or not
+
+                    if (this.status === 200) {
+                        // we used promiseResolved or promiseRejected to determine whether a promise should be resolved or rejected
+                        randomPromiseResolved(giphyRandomRequest.response);
+                    }
+                    else {
+                        randomPromiseRejected(giphyRandomRequest.response)
+                    }
+
                 }
 
+                // opening and sending a request to the gif server
 
+                giphyRandomRequest.open("GET", giphyRandomUrl, true)
+                giphyRandomRequest.send();
+            });
 
-            };
-            // time to open and send my random requests
-            giphyRandomRequest.open("GET", randomGiphyUrl, true);
-            giphyRandomRequest.send();
+            giphyRandomPromise.then(function (randomPromiseSuccess) {
 
+                // parsing the json, so I can be able to access it
+                const giphyBody = JSON.parse(randomPromiseSuccess);
 
-            // function for getting the random gifs and showing them to user
+                // console.log(randomPromiseSuccess);
 
-            function getRandomGifs(myGiphyRandomResponse) {
-                $(".ranGif").show();
-                $(".treGIf").show();
-                console.log(myGiphyRandomResponse.data.embed_url);
+                // showing what I have in my parsed JSON in the console
+                // console.log(giphyBody.data[0].embed_url);
 
-                const secondEmbeddedGiphyUrl = myGiphyRandomResponse.data.embed_url;
+                // parsing the json to store the embed_url image in it
+                const secondEmbeddedGifUrl = giphyBody.data[0].embed_url;
 
-                console.log(secondEmbeddedGiphyUrl);
-
-
-                $(".gif-random-shower").prepend(
+                // showing(in this case prepending) the gifs in the application for user
+                $(".giphy-shower").prepend(
                     `
-                    <br> <br>
-                    <h5>A random gif</h5> 
-                    <iframe src="${secondEmbeddedGiphyUrl}" height="300" width="290" frameborder="0" allowfullscreen>
-                    
-                    </iframe>`
+                <br> <br>
+                    <h5>You are ${userGif}, this is a gif for you</h5> 
+                <iframe src="${secondEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
                 );
-
-            }
-
-
+            },
+                function (myRandomPromiseFailed) {
+                    $(".giphy-shower").prepend(
+                        `
+                <br> <br>
+                <h5>There was an error processing your Request: ${myRandomPromiseFailed}</h5>    
+                <h5>Please Try again</h5>    
+                `
+                    );
+                }
+            )
 
         }
         else if (userPick === "trend") {
 
-            let giphyTrendRequest = new XMLHttpRequest();
-
-            // storing new endpoint in a variable: using the giphy trend endpoint 
-
-            const giphyTrendUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&tag=&rating=g`
-
-
-
-            // NOTE: the 2 new features added don't need 2 parameters like the first one that needed(uservalue and API key)
-            // the only thing the 2 new features need is just the api key, the Team @giphy have already taken care of both the random and trend requests we used
-
-            giphyTrendRequest.onreadystatechange = function () {
-                console.log(this.readyState);
-
-                // a branch to determine if the trend request has been gotten with the giphy API
-
-                if (this.readyState === 4 && this.status === 200) {
-                    const giphyTrendResponse = JSON.parse(this.responseText);
-
-                    getTrendGifs(giphyTrendResponse);
-                }
-
-            };
-
-            // opening and sending the trend request
-
-            giphyTrendRequest.open("GET", giphyTrendUrl, true);
-            giphyTrendRequest.send();
-
-
-            // function for getting the random gifs and showing them to user
-
-            function getTrendGifs(myGiphyTrendResponse) {
-                console.log(myGiphyTrendResponse.data[0].embed_url);
-
-                const thirdEmbeddedGifUrl = myGiphyTrendResponse.data[0].embed_url;
-
-                $(".gif-trend-shower").prepend(
-                    `
-                    <br> <br>
-                    <h5>A Trending gif</h5> 
-                    <iframe src="${thirdEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
-                );
-            }
-
         }
 
-    })
-        */
+    });
+
+
+    /*
+        // Other features by the great giphy App
+    
+        // saving up the 2 new features the app offers
+    
+        $("#other-features").click(function () {
+    
+            let userPick = $("#new-features").find(":selected").val();
+            console.log(userPick);
+    
+            // clearing out the 
+    
+            // running a branch to determine which value user selected to determine which API CALL TO MAKE
+    
+    
+            if (userPick === "random") {
+    
+                // doing the same thing I did for the first app feature by the left 
+                // the only difference is the difference in the API call I'm making
+    
+                let giphyRandomRequest = new XMLHttpRequest();
+    
+                // using another endpoint(the random endpoint): not the endpoint above but a different one 
+                // that will allow me ask giphy for a random gif
+    
+                const randomGiphyUrl = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`
+    
+                giphyRandomRequest.onreadystatechange = function () {
+                    console.log(this.readyState);
+    
+                    if (this.readyState === 4 && this.status === 200) {
+                        const giphyRandomResponse = JSON.parse(this.responseText);
+    
+                        getRandomGifs(giphyRandomResponse);
+                    }
+    
+    
+    
+                };
+                // time to open and send my random requests
+                giphyRandomRequest.open("GET", randomGiphyUrl, true);
+                giphyRandomRequest.send();
+    
+    
+                // function for getting the random gifs and showing them to user
+    
+                function getRandomGifs(myGiphyRandomResponse) {
+                    $(".ranGif").show();
+                    $(".treGIf").show();
+                    console.log(myGiphyRandomResponse.data.embed_url);
+    
+                    const secondEmbeddedGiphyUrl = myGiphyRandomResponse.data.embed_url;
+    
+                    console.log(secondEmbeddedGiphyUrl);
+    
+    
+                    $(".gif-random-shower").prepend(
+                        `
+                        <br> <br>
+                        <h5>A random gif</h5> 
+                        <iframe src="${secondEmbeddedGiphyUrl}" height="300" width="290" frameborder="0" allowfullscreen>
+                        
+                        </iframe>`
+                    );
+    
+                }
+    
+    
+    
+            }
+            else if (userPick === "trend") {
+    
+                let giphyTrendRequest = new XMLHttpRequest();
+    
+                // storing new endpoint in a variable: using the giphy trend endpoint 
+    
+                const giphyTrendUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&tag=&rating=g`
+    
+    
+    
+                // NOTE: the 2 new features added don't need 2 parameters like the first one that needed(uservalue and API key)
+                // the only thing the 2 new features need is just the api key, the Team @giphy have already taken care of both the random and trend requests we used
+    
+                giphyTrendRequest.onreadystatechange = function () {
+                    console.log(this.readyState);
+    
+                    // a branch to determine if the trend request has been gotten with the giphy API
+    
+                    if (this.readyState === 4 && this.status === 200) {
+                        const giphyTrendResponse = JSON.parse(this.responseText);
+    
+                        getTrendGifs(giphyTrendResponse);
+                    }
+    
+                };
+    
+                // opening and sending the trend request
+    
+                giphyTrendRequest.open("GET", giphyTrendUrl, true);
+                giphyTrendRequest.send();
+    
+    
+                // function for getting the random gifs and showing them to user
+    
+                function getTrendGifs(myGiphyTrendResponse) {
+                    console.log(myGiphyTrendResponse.data[0].embed_url);
+    
+                    const thirdEmbeddedGifUrl = myGiphyTrendResponse.data[0].embed_url;
+    
+                    $(".gif-trend-shower").prepend(
+                        `
+                        <br> <br>
+                        <h5>A Trending gif</h5> 
+                        <iframe src="${thirdEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
+                    );
+                }
+    
+            }
+    
+        })
+            */
 });
