69465596c9d82c5e14408ee23d79857952c83c07
Emzzy241 <emzzyoluwole@gmail.com>
2022-11-10 04:36:46 +0100

    adding new file and functionalities for separation of logic

---
+++ src/giphy-service.js
@@ -0,0 +1,19 @@
+// business Logic file
+
+// class for searching giphy based on user's input
+export class SearchGiphyService {
+
+    static getGif(userFeeling) {
+        return new Promise(function (gifResolved, gifRejected) {
+            // making an API request
+            let giphySearchRequest = new XMLHttpRequest();
+
+            // getting giphy's search endpoint url
+
+            let giphySearchUrl = 
+        })
+
+
+    }
+
+}
