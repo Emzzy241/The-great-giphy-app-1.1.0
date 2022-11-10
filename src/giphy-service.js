7327d79db3db1b47b183b70da626e699b73b7393
Emzzy241 <emzzyoluwole@gmail.com>
2022-11-10 04:41:07 +0100

    refactoring code for separation of logics

--- src/giphy-service.js
+++ src/giphy-service.js
@@ -8,10 +8,28 @@ export class SearchGiphyService {
             // making an API request
             let giphySearchRequest = new XMLHttpRequest();
 
-            // getting giphy's search endpoint url
+            // getting giphy's search endpoint url and passing user's value in it
 
-            let giphySearchUrl = 
-        })
+            const giphySearchUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userGif}&limit=1&offset=0&rating=g&lang=en`
+            
+
+            giphyRequest.onload = function () {
+                if (this.status === 200) {
+                    // we used promiseResolved or promiseRejected to determine whether a promise should be resolved or rejected
+                    promiseResolved(giphyRequest.response);
+                }
+                else {
+                    promiseRejected(giphyRequest.response)
+                }
+
+            }
+
+            // opening and sending a request to the gif server
+
+            giphyRequest.open("GET", giphySearchUrl, true);
+            giphyRequest.send();
+        
+        });
 
 
     }
