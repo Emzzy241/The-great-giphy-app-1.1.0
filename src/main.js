1b0e3f3ed9e884333a7fd73cb17163bcb20e084b
Emzzy241 <emzzyoluwole@gmail.com>
2022-11-10 06:21:14 +0100

    updated UI logic file and solved all bugs

--- src/main.js
+++ src/main.js
@@ -6,16 +6,143 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 // importing all three classes to my UI logic
-import {SearchGiphyService} from "./giphy-service.js";
-import {RandomGif} from "./giphy-service.js";
-import {TrendingGif} from "./giphy-service.js";
+import { SearchGiphyService } from "./giphy-service.js";
+import { RandomGif } from "./giphy-service.js";
+import { TrendingGif } from "./giphy-service.js";
 
 
 
 // working with the imported classes
 
-$(document).ready({
-    $("")
+$(document).ready(function () {
+    $(".user-form").submit(function (event) {
+        // preventing the submit button from refreshing 
+        event.preventDefault();
+
+        // taking in the user-emotion value
+
+        const userGif = $("#user-emotion").val();
+        // clearing out the form fields after user submits
+
+        $("#user-emotion").val("");
+
+        console.log(userGif);
+
+        // after importing the SearchGiphyService class, we make our API call by doing this:
+        let searchPromise = SearchGiphyService.getSearchedGif(userGif);
+
+        searchPromise.then(function (searchedResponse) {
+            // parsing the json, so I can be able to access it
+            const giphyBody = JSON.parse(searchedResponse);
+
+            // console.log(searchedResponse);
+
+            // showing what I have in my parsed JSON in the console
+            // console.log(giphyBody.data[0].embed_url);
+
+            // parsing the json to store the embed_url image in it
+            const firstEmbeddedGifUrl = giphyBody.data[0].embed_url;
+
+            // showing(in this case prepending) the gifs in the application for user
+            $(".giphy-shower").prepend(
+                `
+                <br> <br>
+                    <h5>You entered; ${userGif}, here is a gif for you</h5> 
+                <iframe src="${firstEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
+            );
+        },
+            function (myPromiseFailed) {
+                $(".giphy-shower").prepend(
+                    `
+            <br> <br>
+            <h5>There was an error processing your Request: ${myPromiseFailed}</h5>    
+            <h5>Please Try again</h5>    
+            `
+                );
+            }
+
+        )
+    });
+
+
+    $("#other-features").click(function () {
+
+        let userPick = $("#new-features").find(":selected").val();
+        console.log(userPick);
+
+        if (userPick === "random") {
+
+            // now we make our API call like this
+            let randomPromise = RandomGif.getRandomGif();
+
+            randomPromise.then(function (randomPromiseSuccess) {
+                // parsing the json, so I can be able to access it
+                const giphyRandomBody = JSON.parse(randomPromiseSuccess);
+
+                // console.log(randomPromiseSuccess);
+
+                // showing what I have in my parsed JSON in the console
+                // console.log(giphyBody.data.embed_url);
+
+                // parsing the json to store the embed_url image in it
+                const secondEmbeddedGifUrl = giphyRandomBody.data.embed_url;
+
+                // showing(in this case prepending) the gifs in the application for user
+                $(".gif-shower-two").prepend(
+                    `
+                <br> <br>
+                <h5>A Random gif for user</h5> 
+                <iframe src="${secondEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
+                );
+            },
+                function (myRandomPromiseFailed) {
+                    $(".gif-shower-two").prepend(
+                        `
+            <br> <br>
+            <h5>There was an error processing your Request: ${myRandomPromiseFailed}</h5>    
+            <h5>Please Try again</h5>    
+            `
+                    );
+                }
+            )
+
+        }
+        else if (userPick === "trend") {
+
+            // now we make our API call like this
+            let trendingPromise = TrendingGif.getTrendingGif();
+
+            trendingPromise.then(function (trendPromiseSuccess) {
+                // parsing the json gotten
+                const giphyTrendBody = JSON.parse(trendPromiseSuccess);
+
+                const thirdEmbeddedGifUrl = giphyTrendBody.data[0].embed_url;
+
+                // showing(in this case prepending) the gifs in the application for user
+                $(".gif-shower-two").prepend(
+                    `
+                <br> <br>
+                <h5>A Trending gif for user</h5> 
+                <iframe src="${thirdEmbeddedGifUrl}" height="300" width="290" frameborder="0" allowfullscreen></iframe>`
+                );
+
+            },
+                function (myTrendPromiseFailed) {
+                    $(".gif-shower-two").prepend(
+                        `
+                <br> <br>
+                <h5>There was an error processing your Request: ${myTrendPromiseFailed}</h5>    
+                <h5>Please Try again</h5>    
+                `
+                    );
+                }
+            );
+
+        }
+
+    });
+
+
 });
 
 
